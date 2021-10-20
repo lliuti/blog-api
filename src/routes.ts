077c2c40841387d6c94f1daf36a51d1812dc1dbd
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-20 20:51:40 -0300

    Adding CreatePost route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { CreatePostController } from "./controllers/Post/CreatePostController";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
+router.post("/posts", new CreatePostController().handle);
 
 export { router };
