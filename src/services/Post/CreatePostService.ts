2dc407f8cf781edf0bd4e2757efeee716a984d14
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-20 20:51:23 -0300

    Adding CreatePostService

---
+++ src/services/Post/CreatePostService.ts
@@ -0,0 +1,30 @@
+import { IPostDTO } from "../../dtos/IPostDTO";
+import { prismaClient } from "../../prisma";
+
+class CreatePostService {
+  async execute({ title, description, content, tags, user_id }: IPostDTO) {
+    let post = await prismaClient.post.findFirst({
+      where: {
+        title: title,
+      },
+    });
+
+    if (post) {
+      throw new Error("There is already a post with this title!");
+    }
+
+    post = await prismaClient.post.create({
+      data: {
+        title: title,
+        description: description,
+        content: content,
+        tags: tags,
+        author_id: user_id,
+      },
+    });
+
+    return post;
+  }
+}
+
+export { CreatePostService };
