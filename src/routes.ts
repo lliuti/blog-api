5c7f0d1702779c99b1bf59fad0efed98994fe082
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-21 17:48:29 -0300

    adding get recent posts routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreatePostController } from "./controllers/Post/CreatePostController";
+import { GetLastThreePostsController } from "./controllers/Post/GetLastThreePostsController";
 import { AuthenticateUserController } from "./controllers/User/AuthenticateUserController";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
@@ -11,5 +12,6 @@ router.post("/users", new CreateUserController().handle);
 router.post("/auth", new AuthenticateUserController().handle);
 
 router.post("/posts", ensureAuthenticated, new CreatePostController().handle);
+router.get("/posts/recent", new GetLastThreePostsController().handle);
 
 export { router };
