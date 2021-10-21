5e441fbb673bf12cb3986cdd9a8dbbd6cc249203
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-20 21:34:56 -0300

    Adding auth route and ensureAuthenticated middleware to post creation route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreatePostController } from "./controllers/Post/CreatePostController";
+import { AuthenticateUserController } from "./controllers/User/AuthenticateUserController";
 import { CreateUserController } from "./controllers/User/CreateUserController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
-router.post("/posts", new CreatePostController().handle);
+
+router.post("/auth", new AuthenticateUserController().handle);
+
+router.post("/posts", ensureAuthenticated, new CreatePostController().handle);
 
 export { router };
