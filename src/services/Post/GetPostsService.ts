7ec2dd58b7d349db093b2e9b34812fd2d3ed0542
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-21 18:08:58 -0300

    Creating GetPosts service

---
+++ src/services/Post/GetPostsService.ts
@@ -0,0 +1,17 @@
+import { IPostDTO } from "../../dtos/IPostDTO";
+import { prismaClient } from "../../prisma";
+
+class GetPostsService {
+  async execute(): Promise<IPostDTO[]> {
+    const posts = await prismaClient.post.findMany({
+      orderBy: {
+        created_at: "desc",
+      },
+      skip: 3,
+    });
+
+    return posts;
+  }
+}
+
+export { GetPostsService };
