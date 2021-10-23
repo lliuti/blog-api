7b7ac50079b051fac1381f7713a6064790df19a3
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-22 23:38:55 -0300

    Creating github routes

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,14 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 
 router.post("/auth", new AuthenticateUserController().handle);
+router.post("/auth/github", new CreateUserController().handle);
+
+router.get("/github", (request, response) => {
+  response.json({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    redirect_uri: process.env.GITHUB_REDIRECT_URI,
+  });
+});
 
 router.post("/posts", ensureAuthenticated, new CreatePostController().handle);
 router.get("/posts/recent", new GetRecentPostsController().handle);
