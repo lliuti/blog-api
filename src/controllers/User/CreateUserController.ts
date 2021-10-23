28c99cc72055fcfd375d84617ab0b9b3d072bb03
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-22 23:38:13 -0300

    Update CreateUserController.ts

--- src/controllers/User/CreateUserController.ts
+++ src/controllers/User/CreateUserController.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from "express";
-import { IUserDTO } from "../../dtos/IUserDTO";
 import { CreateUserService } from "../../services/User/CreateUserService";
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
-    const { name, email, password }: IUserDTO = request.body;
+    const { code } = request.body;
 
     const service = new CreateUserService();
 
     try {
-      const result = await service.execute({ name, email, password });
+      const result = await service.execute(code);
       return response.status(201).json(result);
     } catch (err) {
       return response.json({ error: err.message });
