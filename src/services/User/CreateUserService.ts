db0c9943658cc01726b3b47e0354468cb8411b90
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-22 23:38:39 -0300

    Creating & authenticating user with github

--- src/services/User/CreateUserService.ts
+++ src/services/User/CreateUserService.ts
@@ -1,28 +1,69 @@
-import { hash } from "bcryptjs";
-import { IUserDTO } from "../../dtos/IUserDTO";
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import { IGithubDTO } from "../../dtos/IGithubDTO";
 import { prismaClient } from "../../prisma";
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
 class CreateUserService {
-  async execute({ name, email, password }: IUserDTO) {
-    const hashedPassword = await hash(password, 8);
+  async execute(code: string) {
+    const accessTokenUrl = `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`;
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(accessTokenUrl, null, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const { access_token } = accessTokenResponse;
+
+    const response = await axios.get<IGithubDTO>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const { login, name, avatar_url } = response.data;
 
     let user = await prismaClient.user.findFirst({
       where: {
-        email: email,
+        github_username: login,
       },
     });
 
     if (!user) {
       user = await prismaClient.user.create({
         data: {
-          name: name,
-          email: email,
-          password: hashedPassword,
+          github_username: login,
+          name,
+          avatar_url,
         },
       });
     }
 
-    return user;
+    const token = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          github_username: user.github_username,
+          name: user.name,
+          avatar_url: user.avatar_url,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+        subject: user.id,
+      }
+    );
+
+    return { token, user };
   }
 }
 
